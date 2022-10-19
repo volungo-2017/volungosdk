a34394fdd5b5a64e2b283aa6423fbe94ab2d1024
˜Felipe <felipe.costa@gmail.com>
2022-10-19 01:33:51 -0300

    volungosdk::endpoint-jobs

---
+++ Jobs/index.js
@@ -0,0 +1,114 @@
+const Axios = require('axios')
+const nookies = require("nookies") ;
+class JobsAPI{
+
+
+  constructor(){
+      this.url = process.env.NEXT_PUBLIC_JOBS_HOST
+  }
+
+      async allJobs(){
+        const cookies = nookies.parseCookies()
+        const token =  cookies['volungo.token']
+         try{
+          const headers = token ? {Authorization: `Bearer ${token}`,'Content-Type': 'application/json'} : {'Content-Type': 'application/json'}
+         return  await Axios.get(`${this.url}/jobs`, {} ,{headers})
+         }
+         catch(err){
+          return err
+         }
+      }
+      async   globalSearch(payload){
+        const cookies = nookies.parseCookies()
+        const token =  cookies['volungo.token']
+         try{
+          const headers = token ? {Authorization: `Bearer ${token}`,'Content-Type': 'application/json'} : {'Content-Type': 'application/json'}
+         return  await Axios.post(`${this.url}/jobs/globalSearch`, payload ,{headers})
+         }
+         catch(err){
+          return err
+         }
+           
+      }
+
+      async searchJob(idJob){
+
+        const cookies = nookies.parseCookies()
+        const token =  cookies['volungo.token']
+         try{
+          const headers = token ? {Authorization: `Bearer ${token}`,'Content-Type': 'application/json'} : {'Content-Type': 'application/json'}
+         return  await Axios.get(`${this.url}/job/${idJob}`, {} ,{headers})
+         }
+         catch(err){
+          return err
+         }
+
+      }
+
+      async search(payload){
+
+        const cookies = nookies.parseCookies()
+        const token =  cookies['volungo.token']
+         try{
+          const headers = token ? {Authorization: `Bearer ${token}`,'Content-Type': 'application/json'} : {'Content-Type': 'application/json'}
+         return  await Axios.post(`${this.url}/jobs/search`, payload ,{headers})
+         }
+         catch(err){
+          return err
+         }
+
+      }
+
+      async Verify(jobID){
+        
+        const cookies = nookies.parseCookies()
+        const token =  cookies['volungo.token']
+         try{
+          const headers = token ? {Authorization: `Bearer ${token}`,'Content-Type': 'application/json'} : {'Content-Type': 'application/json'}
+         return  await Axios.post(`${this.url}/job/apply`, jobID ,{headers})
+         }
+         catch(err){
+          return err
+         }
+
+      }
+
+      async create(payload){
+
+        const cookies = nookies.parseCookies()
+        const token =  cookies['volungo.token']
+         try{
+          const headers = token ? {Authorization: `Bearer ${token}`,'Content-Type': 'application/json'} : {'Content-Type': 'application/json'}
+         return  await Axios.post(`${this.url}/jobs`, payload ,{headers})
+         }
+         catch(err){
+          return err
+         }
+      }
+
+
+
+      async Apply(payload){
+        
+        const cookies = nookies.parseCookies()
+        const token =  cookies['volungo.token']
+         try{
+          const headers = token ? {Authorization: `Bearer ${token}`,'Content-Type': 'application/json'} : {'Content-Type': 'application/json'}
+         return  await Axios.post(`${this.url}/job/application`, payload ,{headers})
+         }
+         catch(err){
+          return err
+         }
+
+
+
+
+      }
+
+     
+  
+  }
+  
+  
+  
+  module.exports = new JobsAPI()
--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 module.exports ={
   AccountAPI:require('./Account'),
   PostAPI:require('./Post'),
+<<<<<<< HEAD
   JobsAPI:require('./jobAll'),
   ChatAPI:require('./Chat')
+=======
+  JobsAPI:require('./Jobs')
+>>>>>>> a970ebe (volungosdk::endpoint-jobs)
 
 }
  
